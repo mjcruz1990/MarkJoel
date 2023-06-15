a0530136e4b53c8a08a27b66e3e63a18e367a09a
mjcruz1990 <135180556+mjcruz1990@users.noreply.github.com>
2023-06-15 17:47:54 +0800

    Add files via upload

---
+++ Delacruz/Dela Cruz/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hi Bby MJ! Kumain ka na?")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If My.Settings.AppStat = "Trial Version" Then
+
+            If My.Settings.LoadCount < 0 Then
+                Form2.ShowDialog()
+            End If
+
+            Me.Text = My.Settings.AppStat & " : Counter =>" & My.Settings.LoadCount
+            My.Settings.LoadCount -= 1
+            My.Settings.Save()
+        Else
+            Me.Text = My.Settings.AppStat
+        End If
+    End Sub
+End Class
---
+++ Delacruz/Dela Cruz/Form2.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form2_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'code: delacruz
+        If My.Settings.SerialKey = TextBox1.Text Then
+            My.Settings.AppStat = "Full Version"
+            My.Settings.Save()
+            Me.Close()
+        Else
+            MsgBox("Invalid Serial Key")
+        End If
+    End Sub
+End Class
